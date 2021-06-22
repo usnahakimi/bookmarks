6d726e2fd2ab839efdffcb7c70571810136d534e
usna.hakimi95@gmail.com <usnahakimi@MA374.local>
2021-06-22 14:23:53 +0100

    creating tables

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,8 +1,8 @@
 feature 'Viewing bookmarks' do 
     scenario 'user can view bookmarks' do
       visit('/bookmarks')
-      expect(page).to have_content "http://wwww.google.com"
-      expect(page).to have_content "http://wwww.bbc.co.uk"
-      expect(page).to have_content "http://wwww.amazon.co.uk"
+      expect(page).to have_content "http://www.google.com"
+      expect(page).to have_content "http://www.bbc.co.uk"
+      expect(page).to have_content "http://www.amazon.co.uk"
     end
 end
